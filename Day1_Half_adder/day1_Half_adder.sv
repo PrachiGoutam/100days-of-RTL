c8376f6320e33dbfbef3686627e64e65ae269e89
Prachi Goutam <122743551+PrachiGoutam@users.noreply.github.com>
2023-05-06 10:04:29 -0700

    Day1_Half_Adder

---
+++ Day1_Half_adder/day1_Half_adder.sv
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+
+
+module day1_Half_adder (
+     input logic a_i,
+     input logic b_i,
+     input logic sum_o,
+     input logic carry_o);
+
+ 
+ assign sum_o = a_i ^ b_i;
+ assign carry_o = a_i & b_i;
+
+endmodule 
